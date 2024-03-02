394e65345369f62221a38ecf8f613bbfda38478d
Dovydas-G <146329425+Dovydas-G@users.noreply.github.com>
2024-03-02 14:55:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const checkBtnDOM = document.querySelector('.check-btn');
+const resetBtnDOM = document.getElementById('reset');
+const resultsDisplayDOM = document.getElementById('result');
+const screenDOM = document.getElementById('screen');
+const weightInputDOM = document.getElementById('weight');
+const heightInputDOM = document.getElementById('height');
+
+checkBtnDOM.addEventListener('click', function () {
+    let count = 0;
+    const weight = weightInputDOM.value;
+    const height = heightInputDOM.value;
+
+    const weightNum = (+ weight);
+    const heightNum = (+ height);
+    
+    if (weight.length === 0 || height.length === 0) {
+        count += 0;
+    } else {
+        count += (+ ((weightNum) / (((heightNum) / 100) ** 2)).toFixed(1));
+    }
+    
+    screenDOM.style.display = 'flex';
+    screenDOM.style.justifyContent = 'space-evenly';
+    
+    let text = '';
+    
+    if (count === 0 || isNaN(count)) {
+        text += 'Please provide positive value !';
+        screenDOM.style.color = 'red';
+        screenDOM.style.backgroundColor = 'transparent';
+        count = '';
+
+    }else {
+        if (count < 18.5) {
+            text += 'Underweight';
+            screenDOM.style.backgroundColor = 'yellow'; 
+
+        }
+        else if (count < 25) {
+            text += 'Normal';
+            screenDOM.style.backgroundColor = 'orange';
+        }
+        else if (count < 30) {
+            text += 'Overweight';
+            screenDOM.style.backgroundColor = '#ff6600';
+        }
+        else {
+            text += 'Obesity'
+            screenDOM.style.backgroundColor = 'red';
+        }
+        
+    }
+    
+    
+    resultsDisplayDOM.innerHTML = `${text} ${count}`;
+    
+});
+
+
+resetBtnDOM.addEventListener('click', function () {
+    screenDOM.style.display = 'none';
+    weightInputDOM.value = '';
+    heightInputDOM.value = '';
+});
